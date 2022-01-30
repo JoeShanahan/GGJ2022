6c21d99926835b11153545966b17e77b0f9ffca5
Joe Shanahan <joe.shanahan@maginteractive.se>
2022-01-30 15:08:48 +0000

    added in some royalty free music

---
+++ Assets/IAamTheMusicMan.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAamTheMusicMan : MonoBehaviour
+{
+
+    static IAamTheMusicMan _instance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
